fb846351b655820fcf327f77fbe5fca50d873d60
Lea Pehar <leapehar@gmail.com>
2022-01-29 07:49:38 +0000

    answered non stretch questions

--- vampire.js
+++ vampire.js
@@ -1,5 +1,5 @@
 class Vampire {
-  constructor(name, yearConverted) {
+  constructor (name, yearConverted) {
     this.name = name;
     this.yearConverted = yearConverted;
     this.offspring = [];
@@ -10,22 +10,38 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
+    this.offspring.push(vampire);
+    vampire.creator = this;
 
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
 
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVapires = 0;
+    let currentVampire = this;
+
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVapires++;
+    }
+
+    return numberOfVapires
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
 
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
@@ -36,6 +52,7 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+
 
   }
 }
