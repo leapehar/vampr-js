29ce0b720be6d0475425ded2a5dc173a8f984266
Lea Pehar <leapehar@gmail.com>
2022-01-29 23:29:05 +0000

    adding new methods

--- vampire.js
+++ vampire.js
@@ -55,6 +55,28 @@ class Vampire {
 
 
   }
+
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+
+
+
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+
+  }
+
+
+
+
+
 }
 
 module.exports = Vampire;
